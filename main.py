c96a58e5ef96224715a475e2c1e9469e5cebd104
Nahidul Islam <97219701+analyticalnahid@users.noreply.github.com>
2022-09-23 12:04:02 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import argparse
 import time
+import glob
 import os
+import re
 from pathlib import Path
 import numpy as np
 
@@ -19,12 +21,29 @@ from utils.plots import plot_one_box
 from utils.torch_utils import select_device, time_synchronized
 
 
-def detect(opt):
-    source, view_img, imgsz, nosave, show_conf, save_path, show_fps = opt.source, not opt.hide_img, opt.img_size, opt.no_save, not opt.hide_conf, opt.output_path, opt.show_fps
+
+def increment_path(path, exist_ok=True, sep=''):
+    # Increment path, i.e. runs/exp --> runs/exp{sep}0, runs/exp{sep}1 etc.
+    path = Path(path)  # os-agnostic
+    if (path.exists() and exist_ok) or (not path.exists()):
+        return str(path)
+    else:
+        dirs = glob.glob(f"{path}{sep}*")  # similar paths
+        matches = [re.search(rf"%s{sep}(\d+)" % path.stem, d) for d in dirs]
+        i = [int(m.groups()[0]) for m in matches if m]  # indices
+        n = max(i) + 1 if i else 2  # increment number
+        return f"{path}{sep}{n}"  # update path
+
+
+
+def detect(save_img=False):
+    source, weights, save_txt, show_conf, imgsz, trace = opt.source, opt.weights, opt.save_txt, not opt.hide_conf, opt.img_size, not opt.no_trace
+    save_img = not opt.nosave and not source.endswith('.txt')  # save inference images
     webcam = source.isnumeric() or source.endswith('.txt') or source.lower().startswith(
         ('rtsp://', 'rtmp://', 'http://', 'https://'))
     # Directories
-    create_folder(save_path)
+    save_dir = Path(increment_path(Path(opt.project) / opt.name, exist_ok=opt.exist_ok))  # increment run
+    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True) 
 
     # Initialize
     set_logging()
@@ -33,7 +52,8 @@ def detect(opt):
     half = device.type != 'cpu'  # half precision only supported on CUDA
 
     # Load model
-    model = attempt_load("weights/yolo.pt", map_location=device)  # load FP32 model
+    model = attempt_load(weights, map_location=device)  # load FP32 model
+    #model = attempt_load("weights/yolo.pt", map_location=device)  # load FP32 model
     stride = int(model.stride.max())  # model stride
     imgsz = check_img_size(imgsz, s=stride)  # check img_size
     if half:
@@ -79,7 +99,8 @@ def detect(opt):
                 p, s, im0, frame = path, '', im0s.copy(), getattr(dataset, 'frame', 0)
 
             p = Path(p)  # to Path
-            s += '%gx%g ' % img.shape[2:]  # print string
+            save_path = str(save_dir / p.name)  # img.jpg
+            txt_path = str(save_dir / 'labels' / p.stem) + ('' if dataset.mode == 'image' else f'_{frame}')  # img.txt
             gn = torch.tensor(im0.shape)[[1, 0, 1, 0]]  # normalization gain whwh
             if len(det):
                 # Rescale boxes from img_size to im0 size
@@ -89,6 +110,7 @@ def detect(opt):
                 for c in det[:, -1].unique():
                     n = (det[:, -1] == c).sum()  # detections per class
                     s += f"{n} {names[int(c)]}{'s' * (n > 1)}, "  # add to string
+                
                 images = []
                 for *xyxy, conf, cls in reversed(det):
                     x1, y1, x2, y2 = xyxy
@@ -98,26 +120,33 @@ def detect(opt):
                     emotions = detect_emotion(images,show_conf)
                 # Write results
                 i = 0
-                for *xyxy, conf, cls in reversed(det):
 
-                    if view_img or not nosave:  
-                        # Add bbox to image with emotions on 
+                # Write results
+                for *xyxy, conf, cls in reversed(det):
+                    if save_txt:  # Write to file
+                        xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                        line = (cls, *xywh, conf) if opt.save_conf else (cls, *xywh)  # label format
+                        with open(txt_path + '.txt', 'a') as f:
+                            f.write(('%g ' * len(line)).rstrip() % line + '\n')
+                    
+                    if save_img or view_img:  # Add bbox to image
                         label = emotions[i][0]
                         colour = colors[emotions[i][1]]
-                        i += 1
-                        plot_one_box(xyxy, im0, label=label, color=colour, line_thickness=opt.line_thickness)
+                        plot_one_box(xyxy, im0, label=label, color=colour, line_thickness=1)        
+                
+                 
 
 
             # Stream results
-            if view_img:
-                display_img = cv2.resize(im0, (im0.shape[1]*2,im0.shape[0]*2))
-                cv2.imshow("Emotion Detection",display_img)
-                cv2.waitKey(1)  # 1 millisecond
-            if not nosave:
-                # check what the output format is
-                ext = save_path.split(".")[-1]
-                if ext in ["mp4","avi"]:
-                    # Save results (image with detections)
+            #if view_img:
+              #  display_img = cv2.resize(im0, (im0.shape[1]*2,im0.shape[0]*2))
+              #  cv2.imshow("Emotion Detection",display_img)
+                #cv2.waitKey()  # 1 millisecond
+            if save_img:
+                if dataset.mode == 'image':
+                    cv2.imwrite(save_path, im0)
+                    print(f" The image with the result is saved in: {save_path}")
+                else:  # 'video' or 'stream'
                     if vid_path != save_path:  # new video
                         vid_path = save_path
                         if isinstance(vid_writer, cv2.VideoWriter):
@@ -128,40 +157,50 @@ def detect(opt):
                             h = int(vid_cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
                         else:  # stream
                             fps, w, h = 30, im0.shape[1], im0.shape[0]
+                            save_path += '.mp4'
                         vid_writer = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                     vid_writer.write(im0)
-                elif ext in ["bmp", "pbm", "pgm", "ppm", "sr", "ras", "jpeg", "jpg", "jpe", "jp2", "tiff", "tif", "png"]:
-                    # save image
-                    cv2.imwrite(save_path,im0)
-                else:
-                    # save to folder
-                    output_path = os.path.join(save_path,os.path.split(path)[1])
-                    create_folder(output_path)
-                    cv2.imwrite(output_path,im0)
 
-        if show_fps:
-            # calculate and display fps
-            print(f"FPS: {1/(time.time()-t0):.2f}"+" "*5,end="\r")
-            t0 = time.time()
+    if save_txt or save_img:
+        s = f"\n{len(list(save_dir.glob('labels/*.txt')))} labels saved to {save_dir / 'labels'}" if save_txt else ''
+        #print(f"Results saved to {save_dir}{s}")
+
+    print(f'Done. ({time.time() - t0:.3f}s)')       
         
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument('--weights', nargs='+', type=str, default='weights/yolo.pt', help='model.pt path(s)')
     parser.add_argument('--source', type=str, default='0', help='source')  # file/folder, 0 for webcam
     parser.add_argument('--img-size', type=int, default=512, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.5, help='face confidence threshold')
     parser.add_argument('--iou-thres', type=float, default=0.45, help='IOU threshold for NMS')
     parser.add_argument('--device', default='', help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
+
+    parser.add_argument('--save-txt', action='store_true', help='save results to *.txt')
+    parser.add_argument('--save-conf', action='store_true', help='save confidences in --save-txt labels')
     parser.add_argument('--hide-img', action='store_true', help='hide results')
+    
+    parser.add_argument('--project', default='runs/detect', help='save results to project/name')
+    parser.add_argument('--name', default='exp', help='save results to project/name')
+    parser.add_argument('--exist-ok', action='store_true', help='existing project/name ok, do not increment')
+    parser.add_argument('--update', action='store_true', help='update all models')
+    parser.add_argument('--nosave', action='store_true', help='do not save images/videos')
     save = parser.add_mutually_exclusive_group()
     save.add_argument('--output-path', default="output.mp4", help='save location')
-    save.add_argument('--no-save', action='store_true', help='do not save images/videos')
     parser.add_argument('--agnostic-nms', action='store_true', help='class-agnostic NMS')
     parser.add_argument('--augment', action='store_true', help='augmented inference')
+    parser.add_argument('--no-trace', action='store_true', help='don`t trace model')
     parser.add_argument('--line-thickness', default=2, type=int, help='bounding box thickness (pixels)')
     parser.add_argument('--hide-conf', default=False, action='store_true', help='hide confidences')
     parser.add_argument('--show-fps', default=False, action='store_true', help='print fps to console')
     opt = parser.parse_args()
     check_requirements(exclude=('pycocotools', 'thop'))
+
     with torch.no_grad():
-        detect(opt=opt)
+        if opt.update:  # update all models (to fix SourceChangeWarning)
+            for opt.weights in ['weights/yolo.pt']:
+                detect(opt=opt)
+                strip_optimizer(opt.weights)
+        else:
+            detect()
